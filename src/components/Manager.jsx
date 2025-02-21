566b1bdec26ffe842f5aee8ca0c90ab18cb39f7f
Shikhaar <shikharsrivastava3004@gmail.com>
2025-02-21 16:15:13 +0530

    Some changes

--- src/components/Manager.jsx
+++ src/components/Manager.jsx
@@ -4,8 +4,14 @@ const Manager = () => {
   return (
   <>
    <div class="absolute inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]"><div class="absolute left-0 right-0 top-0 -z-10 m-auto h-[310px] w-[310px] rounded-full bg-fuchsia-400 opacity-20 blur-[100px]"></div></div>
-      <div className="mx-auto bg-slate-800 max-w-4xl">
+      <div className="mx-auto bg-slate-50 myconatiner">
+        <h1 className='text-4xl text font-bold text-center'>
+          <span className='text-green-700'> &lt;</span>
 
+          <span>Pass</span><span className='text-green-700'>OP/&gt;
+          </span>   
+        </h1>
+        <p classname='text-green-900 text-lg text-center'>Your own password manager</p>
      <div className="text-white flex flex-col p-4">
         <input className='rounded-full' type="text" name="" id="" />
          <div classNAme="flex">
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,8 +2,15 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <nav className='bg-purple-200 flex justify-between items-center px-4 h-24'>
-        <div className="logo font-bold">Passop</div>
+    <nav className='bg-purple-200 '>
+        <div className='myconatiner flex justify-between items-center px-4 py-5 h-14'>
+
+        <div className="logo font-bold text-white text-2xl">
+          <span className='text-green-700'> &lt;</span>
+
+          <span>Pass</span><span className='text-green-700'>OP/&gt;
+          </span>
+          </div>
       <ul>
         <li className='flex gap-4 '>
             <a className='hover:font-bold' href='/'>Home</a>
@@ -11,6 +18,7 @@ const Navbar = () => {
             <a className='hover:font-bold' href='#'>Contact</a>
         </li>
       </ul>
+      </div>
     </nav>
   )
 }
